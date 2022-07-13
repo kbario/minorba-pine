06bbb9120c6cd76711596ed8cb060bb6bac3f299
kbario <kylebario1@gmail.com>
2022-07-13 17:50:38 +0800

    trying to add post size

---
+++ src/app/pages/stock/posts.ts
@@ -0,0 +1,20 @@
+export interface diameterAndNumber {
+  diameter: number;
+  number: number;
+}
+
+export const shorterSizes: diameterAndNumber[] = [
+  { diameter: 80, number: 100 },
+  { diameter: 100, number: 60 },
+  { diameter: 120, number: 42 },
+  { diameter: 140, number: 28 },
+  { diameter: 160, number: 21 },
+  { diameter: 180, number: 16 },
+  { diameter: 200, number: 12 },
+  { diameter: 225, number: 9 },
+  { diameter: 250, number: 7 },
+];
+
+export const longDiameters: diameterAndNumber[] = [
+  { diameter: 120, number:  },
+];
--- src/app/pages/stock/stock.component.ts
+++ src/app/pages/stock/stock.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { posts } from './posts';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.css']
+  styleUrls: ['./stock.component.css'],
 })
-export class StockComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class StockComponent {
+  public readonly posts: posts[] = [
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 100, length: 1.8, price: 200, number: 60 },
+    { diameter: 120, length: 1.8, price: 200, number: 42 },
+    { diameter: 140, length: 1.8, price: 200, number: 28 },
+    { diameter: 160, length: 1.8, price: 200, number: 21 },
+    { diameter: 180, length: 1.8, price: 200, number: 16 },
+    { diameter: 200, length: 1.8, price: 200, number: 12 },
+    { diameter: 225, length: 1.8, price: 200, number: 9 },
+    { diameter: 250, length: 1.8, price: 200, number: 7 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+    { diameter: 80, length: 1.8, price: 200, number: 90 },
+  ];
 }
